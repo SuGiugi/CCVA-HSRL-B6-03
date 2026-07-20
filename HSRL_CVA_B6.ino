cb13a33b141d73cd2bcf0df0104e47656a2a7162
SuGiugi <beducviet19@gmail.com>
2026-07-20 18:38:18 +0700

    fix: vòng 2 đi line bên trái
    không tính về bãi đỗ xe 2p52

--- HSRL_CVA_B6.ino
+++ HSRL_CVA_B6.ino
@@ -2,40 +2,45 @@
 #include <algorithm> 
 
 unsigned int camData[10];
-unsigned int redData[10];
-unsigned int greenData[10];
-int camStatus;
-int startDegrees;
 float last_error = 0;
 int color_id, x, y, last_color_id, cnt = 0, line_color = 10, step = 0;
 bool final = false;
 
-const int Y_IGNOR = 100;
-const int GREEN = 310;
-const int RED = 5;
-const int DOOR = 12;
+// Hằng số
+const int Y_IGNOR = 70; // Vị trí tối thiểu để đọc khối theo trục Y (nếu xa hơn sẽ không đọc)
+const int GREEN = 310; // Vị trí né khối theo trục x của màu xanh
+const int RED = 5; // Vị trí né khối theo trục y của màu đỏ
+const int DOOR = 12; // Điều kiện kết thúc vòng chạy
 
 
 
+// Giới hạn góc xoay
 float limit(float value, float min, float max) {
   if (value < min) value = min;
   if (value > max) value = max;
   return value;
 }
 
-void servoMotor(float value, float l = 53) {
+/*
+  Tính toán vị trí xoay
+  value: góc muốn xoay (0 = đi thẳng)
+  l: giới hạn góc xoay
+*/
+void servoMotor(float value, float l = 50) {
   float diff = 90 + limit(value, -l, l);
   MiniR4.RC1.setAngle(diff);
 }
 
+// dò đường trái
 void doduong_laser_trai(float khoang_cach, float kp, float kd) {
   float error = khoang_cach - MiniR4.I2C1.MXLaserV2.getDistance() / 10;
   float D = last_error - error;
   float PID = error * kp + D * kd;
-  servoMotor(PID);
+  servoMotor(PID, 40);
   last_error = error;
 }
 
+// dò đường phải
 void doduong_laser_phai(float khoang_cach, float kp, float kd) {
   float error = MiniR4.I2C2.MXLaserV2.getDistance() / 10  - khoang_cach;
   float D = last_error - error;
@@ -44,6 +49,7 @@ void doduong_laser_phai(float khoang_cach, float kp, float kd) {
   last_error = error;
 }
 
+// Kiểm tra điều kiện kết thúc nếu: đi đủ khoảng cách + chạm đường xanh cam hoặc vòng đầu tiên + chạm đường xanh cam
 void line_check() {
   line_color = MiniR4.I2C3.MXColorV3.getColorID();
 
@@ -53,27 +59,30 @@ void line_check() {
   }
 }
 
+// Khi đi tới vị trí góc sa bàn (ở giữa 2 đường xanh cam) robot sẽ xoay 1 góc rộng để quét các khối tiếp theo (nếu có)
 bool turn() {
-  bool check = false;
+  bool check = false; // Kiểm tra xem đã đến góc sa bàn chưa
     if (line_color == 9 and cnt != DOOR) {
-      check = true;
+    last_error = 0; 
+    check = true;
     MiniR4.LED.setColor(1, 255,255, 0);
       color_id = camData[0];
       y = camData[2];
       if (y < Y_IGNOR) color_id = 255;
-
+      // Nếu không có khối nào tiếp theo
       if (color_id == 255) {
         MiniR4.LED.setColor(1, 255, 0, 255);
         unsigned int timeStart = millis();
-        int turn_angle = -45;
+        // Vừa đi vừa xoay 1 góc rộng ví dụ -15 -> 47 để quét xem liệu có màu để bám theo ko
+        int turn_angle = 0;
         while (line_color != 3 and color_id == 255 and millis() - timeStart < 4000) {
           line_check();
 
           MiniR4.Vision.Data(camData);
 
           servoMotor(turn_angle);
-          if (turn_angle <= 50) {
-            turn_angle += 5;
+          if (turn_angle <= 42) {
+            turn_angle += 12;
           }
 
           color_id = camData[0];
@@ -84,7 +93,7 @@ bool turn() {
             break;
           } 
 
-          MiniR4.M1.setSpeed(15);
+          MiniR4.M1.setSpeed(20);
           line_color = MiniR4.I2C3.MXColorV3.getColorID();
         }
 
@@ -107,6 +116,30 @@ bool turn() {
   return check;
 }
 
+// Một khối chờ cho việc đi mù nếu gặp được khối nào đó thì sẽ bỏ qua và trực tiếp bám khối
+bool dichuyen_cm(float quang_duong) {
+  last_error = 0;
+  int startDegrees = abs(MiniR4.M1.getDegrees());
+  while(abs(MiniR4.M1.getDegrees() - startDegrees) *19.6 / 360 <= quang_duong) {
+    if(turn()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void last_step(bool check = true) {
+  if (final) {
+    if (dichuyen_cm(last_color_id == 0 ? 13 : 13) && last_color_id == 1 && check) {
+      servoMotor(-45);
+      dichuyen_cm(11);
+    }
+    final = false;
+    last_error = 0;
+    last_color_id = 255;
+  } 
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -119,21 +152,21 @@ void setup() {
   MiniR4.I2C3.MXColorV3.begin();
   MiniR4.Vision.Begin();
   MiniR4.Vision.reset();
-  while (!MiniR4.BTN_UP.getState());
+  
+  while (true) {
+    if (MiniR4.BTN_DOWN.getState()) {
+      while(1) {
+        MiniR4.Vision.Data(camData);
+      }
+    } else if (MiniR4.BTN_UP.getState()) {
+      break;
+    }
+  };
   MiniR4.Vision.Data(camData);
 }
 
-bool dichuyen_cm(float quang_duong) {
-  int startDegrees = abs(MiniR4.M1.getDegrees());
-  while(abs(MiniR4.M1.getDegrees() - startDegrees) *19.6 / 360 <= quang_duong) {
-    if(turn()) {
-      return false;
-    }
-  }
-  return true;
-}
-
 void loop() {
+
   MiniR4.Vision.Data(camData);
   color_id = camData[0];
   x = camData[1];
@@ -142,45 +175,36 @@ void loop() {
   MiniR4.M1.setSpeed(25);
   
   line_color = MiniR4.I2C3.MXColorV3.getColorID();
-
   turn();
 
 
-  if (camData[0] != 255 and y < 120) {
+  if (color_id != 255 and y < 120) {
+      last_step(false);
       MiniR4.LED.setColor(1, 0, 0, 255);
-      servoMotor(int(camData[1] - 160) * 0.6, 30);
-      MiniR4.M1.setSpeed(30);
+      servoMotor(int(x - 160) * 0.6, 30);
+      MiniR4.M1.setSpeed(20);
       step = 1;
-  } else if (camData[0] == 0 && step == 1) {
+      final = false;
+  } else if (color_id == 0 && step == 1) {
       // MiniR4.M1.setBrake(true);
       // while(1);
 
       MiniR4.LED.setColor(1, 0, 255, 0);
-      MiniR4.M1.setSpeed(20);
-      servoMotor(int(camData[1] - GREEN) * 0.5, 36);
+      MiniR4.M1.setSpeed(15);
+      servoMotor(int(x - GREEN) * 0.5, 36);
       final = true;
       last_color_id = 0;
-  } else if (camData[0] == 1 && step == 1) {
+  } else if (color_id == 1 && step == 1) {
       MiniR4.LED.setColor(1, 255, 0, 0);
-      MiniR4.M1.setSpeed(20);
-      servoMotor(int(camData[1] - RED) * 0.6, 36);
+      MiniR4.M1.setSpeed(15);
+      servoMotor(int(x - RED) * 0.6, 36);
       final = true;
       last_color_id = 1;
-  } else if (camData[0] == 255 and line_color == 10) {
-    if (final) {
-
-      // MiniR4.M1.setBrake(true);
-      // while(1);
-      if (dichuyen_cm(last_color_id == 0 ? 15 : 15) && last_color_id == 1) {
-        servoMotor(-50);
-        dichuyen_cm(10);
-      }
-      final = false;
-      last_error = 0;
-    } // xet trong mau trang
+  } else if (color_id == 255 and line_color == 10) {
+    last_step();
     MiniR4.LED.setColor(1, 0, 0, 0);
-    MiniR4.M1.setSpeed(40);
-    doduong_laser_trai(60, 1.2, 1);
+    MiniR4.M1.setSpeed(45);
+    doduong_laser_trai(60, 1.4, 2);
     step = 0;
   } 
 }
--- OpenMV/main.py
+++ OpenMV/main.py
@@ -13,56 +13,53 @@ sensor.set_hmirror(True)
 
 fill_light.on()
 
-threshold_obj = [(37, 18, -128, -15, 127, -128), (19, 9, 19, 127, -128, 67), (36, 5, 28, 127, -128, 31)]
+threshold_obj = [(26, 18, -128, -15, 127, -128), (38, 4, 55, 127, -128, 52), (30, 6, 22, 127, -128, 31), (30, 8, -128, -14, 127, -128)]
 clock = time.clock()
 nearest_data = (-1, 0, 0, 0)
 def finding(color_id):
     global nearest_data
-    if color_id == 2:
-        blobs = img.find_blobs([threshold_obj[color_id]], pixels_threshold=700, area_threshold=200)
+    if color_id == 3:
+        blobs = img.find_blobs([threshold_obj[color_id]], pixels_threshold=300, area_threshold=200)
+    elif color_id == 2:
+        blobs = img.find_blobs([threshold_obj[color_id]], pixels_threshold=800, area_threshold=200)
     else:
-        blobs = img.find_blobs([threshold_obj[color_id]], pixels_threshold=600, area_threshold=200)
-    if color_id == 2:
-        color_id = 1
+        blobs = img.find_blobs([threshold_obj[color_id]], pixels_threshold=400, area_threshold=200)
     if blobs:
         max_blob = max(blobs, key=lambda b: b.cy())
         img.draw_rectangle(max_blob.rect())
         x_center = max_blob.cx()
         y_center = max_blob.cy()
         blob_area = round(max_blob.area() / 2)
-
+        img.draw_cross(x_center, y_center)
+        img.draw_string(max_blob.x(), max_blob.y() - 15, f"C:{color_id}, X:{x_center}, Y:{y_center}", color=(255, 255, 255))
+        if color_id == 2:
+            color_id = 1
+        if color_id == 3:
+            color_id = 0
         if nearest_data[2] <= y_center:
             nearest_data = (color_id, x_center, y_center, blob_area)
-
-        img.draw_cross(x_center, y_center)
-        img.draw_string(max_blob.x(), max_blob.y() - 15, f"X:{x_center}, Y:{y_center}", color=(255, 255, 255))
-
 
 # while True:
 #     fill_light.brightness(100)
 #     # Kiểm tra xem robot có gửi lệnh yêu cầu lấy dữ liệu không
-#     clock.tick()
-
-#     # CHỤP ẢNH NGAY TẠI THỜI ĐIỂM ROBOT YÊU CẦU
+#             # CHỤP ẢNH NGAY TẠI THỜI ĐIỂM ROBOT YÊU CẦU
 #     img = sensor.snapshot()
 
 #     nearest_data = (-1, 0, 0, 0)
 #     finding(0)
 #     finding(1)
-#     finding(2)
-#     # finding(3)
-
+#     # finding(2)
+#     finding(3)
 #     # Gửi dữ liệu mới nhất về ngay lập tức
 #     if nearest_data != (-1, 0, 0, 0):
 #         send_data([nearest_data[0], nearest_data[1], nearest_data[2], nearest_data[3]])
 #     else:
 #         # Nếu không thấy vật thể, gửi gói dữ liệu trống để robot không bị đợi timeout
 #         send_data([255, 0, 0, 0])
-#     # Thêm delay nhỏ để giảm tải cho CPU camera khi đứng đợi lệnh
 
 
 while True:
-    fill_light.brightness(100)
+    fill_light.brightness(0)
     # Kiểm tra xem robot có gửi lệnh yêu cầu lấy dữ liệu không
     if uart.any():
         cmd = uart.read(1) # Đọc 1 byte lệnh từ robot
@@ -74,9 +71,9 @@ while True:
 
             nearest_data = (-1, 0, 0, 0)
             finding(0)
-            finding(1)
+            # finding(1)
             finding(2)
-            # finding(3)
+            finding(3)
             # Gửi dữ liệu mới nhất về ngay lập tức
             if nearest_data != (-1, 0, 0, 0):
                 send_data([nearest_data[0], nearest_data[1], nearest_data[2], nearest_data[3]])
